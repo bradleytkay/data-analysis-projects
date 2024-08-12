6bcdee881fe4fee0882fccbada7d6137a360b5a9
Brad Kay <bradleytkay@Brads-MacBook-Air.local>
2024-08-12 17:28:23 -0500

    assignments in reading completed

--- loops/exercises/for-loop-exercises.py
+++ loops/exercises/for-loop-exercises.py
@@ -5,4 +5,35 @@
     # d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers).
 
 
+#a
+for num in range(0, 21, 1):
+    print(num); 
+
+#b
+for num in range(3, 29, 3):
+    if num % 2 == 1:
+        print(num); 
+
+#c
+num = 12
+decrease_by = 2
+
+while num <= 12:
+    num -= decrease_by
+    print(num)
+
+    if num <= -14:
+        break; 
+
+#d
+num = 50
+decrease_by = 1
+
+while num <= 50:
+    num -= decrease_by
+    if num % 3 == 0:
+        print(num)
+
+    if num <= -20:
+        break;
 
